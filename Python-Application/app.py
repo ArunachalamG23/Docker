e7df37a55135ce73385921b3dca1664df5c7a6b8
Arunachalam.G <110539096+ArunachalamG23@users.noreply.github.com>
2025-05-12 10:41:30 +0530

    Update app.py

--- Python-Application/app.py
+++ Python-Application/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Second Phase in a better way'
+    return 'Third Phase in a better way'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
